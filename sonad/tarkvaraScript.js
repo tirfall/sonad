c7b43a4cb56041837f6bf6d39bba11cc92e53a6e
Aleksander Rogovski <61885744+tirfall@users.noreply.github.com>
2024-06-11 12:19:26 +0300

    [#187304258] - Uus js script

--- sonad/tarkvaraScript.js
+++ sonad/tarkvaraScript.js
@@ -32,6 +32,7 @@ const synadVene = [
     'итерация',
     'исправление'
 ];
+let currentSyna = '';
 //random sõna - arv
 
 
@@ -49,5 +50,22 @@ function kontroll() {
         document.getElementById("kontrollDiv").innerText = "õigus";
     } else {
         document.getElementById("kontrollDiv").innerText = "vale";
+    }
+}
+
+function randomSynaVene() {
+    const juhuslikSyna = Math.floor(Math.random() * synadVene.length);
+    currentSyna = synadVene[juhuslikSyna];
+    document.getElementById("random-synaVene").innerHTML = currentSyna;
+}
+
+function kontrollVene() {
+    const inputValue = document.getElementById("kontrollVene").value;
+    const index = synadVene.indexOf(currentSyna);
+
+    if (synadEst[index] === inputValue) {
+        document.getElementById("kontrollDivVene").innerText = "õigus";
+    } else {
+        document.getElementById("kontrollDivVene").innerText = "vale";
     }
 }
